0df88125e3a8a57c4a5e93f11ca38a40049f334a
Claus Marxmeier <claus@marxmeier.de>
2017-06-11 12:24:47 +0200

    Create site.mk

--- site.mk
+++ site.mk
@@ -1,23 +1,22 @@
 GLUON_SITE_PACKAGES := \
     gluon-alfred \
     gluon-autoupdater \
-    gluon-banner \
-    gluon-check-client-mesh \
-    gluon-check-mesh \
     gluon-config-mode-autoupdater \
     gluon-config-mode-contact-info \
     gluon-config-mode-core \
     gluon-config-mode-geo-location \
     gluon-config-mode-hostname \
+    gluon-banner \
+    gluon-check-client-mesh \
+    gluon-check-mesh \
     gluon-config-mode-tunneldigger \
     gluon-ebtables-filter-multicast \
     gluon-ebtables-filter-ra-dhcp \
-    gluon-luci-admin \
-    gluon-luci-autoupdater \
-    gluon-luci-node-role \
-    gluon-luci-portconfig \
-    gluon-luci-private-wifi \
-    gluon-luci-wifi-config \
+    gluon-web-admin \
+    gluon-web-autoupdater \
+    gluon-web-network \
+    gluon-web-private-wifi \
+    gluon-web-wifi-config \
     gluon-mesh-batman-adv-15 \
     gluon-authorized-keys \
     gluon-mesh-vpn-tunneldigger \
@@ -25,7 +24,6 @@ GLUON_SITE_PACKAGES := \
     gluon-next-node \
     gluon-radvd \
     gluon-respondd \
-    gluon-rfkill-disable \
     gluon-setup-mode \
     gluon-ssid-changer \
     gluon-status-page \
@@ -38,7 +36,7 @@ GLUON_SITE_PACKAGES := \
     gluon-treatment
 
 
-DEFAULT_GLUON_RELEASE := 2016.2.5-stable-2.9
+DEFAULT_GLUON_RELEASE := 2017.1-stable-2.9
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
