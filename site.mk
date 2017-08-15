89dd120131fa77f6a07b69ac298aff55df32e87b
Claus Marxmeier <claus@marxmeier.de>
2017-08-15 09:31:27 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -35,7 +35,7 @@ GLUON_SITE_PACKAGES := \
     rsk-nightswitch \
     gluon-rsk-config 
     
-DEFAULT_GLUON_RELEASE := stable-2.8.5
+DEFAULT_GLUON_RELEASE := stable-2.8.6
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
