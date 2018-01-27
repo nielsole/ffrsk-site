f469a161c5722900bb58384d3cca71266d2e5d56
Claus Marxmeier <claus@marxmeier.de>
2018-01-27 19:22:44 +0100

    Create site.mk

---
+++ site.mk
@@ -0,0 +1,50 @@
+GLUON_SITE_PACKAGES := \
+    gluon-alfred \
+    gluon-autoupdater \
+    gluon-config-mode-autoupdater \
+    gluon-config-mode-contact-info \
+    gluon-config-mode-core \
+    gluon-config-mode-geo-location \
+    gluon-config-mode-autoupdater \
+    gluon-config-mode-hostname \
+    gluon-config-mode-mesh-vpn \
+    gluon-core \
+    gluon-banner \
+    gluon-check-client-mesh \
+    gluon-check-mesh \
+    gluon-ebtables \
+    gluon-ebtables-filter-multicast \
+    gluon-ebtables-filter-ra-dhcp \
+    gluon-ebtables-source-filter \
+    gluon-web-admin \
+    gluon-web-autoupdater \
+    gluon-web-network \
+    gluon-web-private-wifi \
+    gluon-web-wifi-config \
+    gluon-mesh-batman-adv-15 \
+    gluon-mesh-vpn-fastd \
+    gluon-neighbour-info \
+    gluon-radvd \
+    gluon-respondd \
+    gluon-setup-mode \
+    gluon-ssid-changer \
+    gluon-status-page \
+    haveged \
+    iptables \
+    iwinfo \
+    rsk-nightswitch \
+    gluon-rsk-block-mesh \
+    gluon-rsk-config \
+    rsk-robinson
+
+
+DEFAULT_GLUON_RELEASE := stable-2.9.10
+# Allow overriding the release number from the command line
+
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de en
+GLUON_REGION ?= eu
+# Build ATH10K images
+GLUON_ATH10K_MESH ?= 11s
+GLUON_WLAN_MESH ?= 11s
