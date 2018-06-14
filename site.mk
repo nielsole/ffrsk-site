89fcc857b72b2a0c2c6bc32b483242b478897978
Claus Marxmeier <claus@marxmeier.de>
2018-06-14 12:28:48 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -25,6 +25,7 @@ GLUON_SITE_PACKAGES := \
     gluon-neighbour-info \
     gluon-radvd \
     gluon-respondd \
+    respondd-module-airtime \
     gluon-setup-mode \
     gluon-ssid-changer \
     gluon-status-page \
@@ -37,7 +38,7 @@ GLUON_SITE_PACKAGES := \
     gluon-rsk-config
 
 
-DEFAULT_GLUON_RELEASE := stable-2.9.16
+DEFAULT_GLUON_RELEASE := stable-2.9.17
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
