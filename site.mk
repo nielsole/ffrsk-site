e615016f33b6b5e9fceae9541c05f1f17744e84d
Claus Marxmeier <claus@marxmeier.de>
2018-06-17 21:12:29 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -38,7 +38,7 @@ GLUON_SITE_PACKAGES := \
     gluon-rsk-config
 
 
-DEFAULT_GLUON_RELEASE := stable-2.9.17
+DEFAULT_GLUON_RELEASE := stable-2.9.18
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
