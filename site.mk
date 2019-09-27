9bf055f013033739630f1aa662c2b7ec8363d343
Claus Marxmeier <claus@marxmeier.de>
2019-09-27 12:24:54 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -43,7 +43,7 @@ GLUON_SITE_PACKAGES := \
     rsk-robinson
 
 
-DEFAULT_GLUON_RELEASE := stable-3.0.8
+DEFAULT_GLUON_RELEASE := stable-3.1.0
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
@@ -53,3 +53,6 @@ GLUON_REGION ?= eu
 # Build ATH10K images
 GLUON_ATH10K_MESH ?= 11s
 GLUON_WLAN_MESH ?= 11s
+
+# Do build images for deprecated devices
+GLUON_DEPRECATED ?= 1
