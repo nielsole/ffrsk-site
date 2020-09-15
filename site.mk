041c801539a14403e4187d28a788ec3aa1675e06
Claus Marxmeier <claus@marxmeier.de>
2020-09-15 09:08:47 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -42,7 +42,7 @@ GLUON_SITE_PACKAGES := \
     rsk-robinson
 
 
-DEFAULT_GLUON_RELEASE := stable-3.4.3
+DEFAULT_GLUON_RELEASE := stable-3.4.4
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
